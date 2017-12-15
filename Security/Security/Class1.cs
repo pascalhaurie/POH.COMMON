bdc32de1afdfce972ab95095eb954db6ca10649a
Pascal Haurie <pascal.haurie@hotmail.com>
2017-12-16 00:28:36 +0100

    Navbars

--- Security/Security/Class1.cs
+++ Security/Security/Class1.cs
@@ -4,9 +4,15 @@ namespace Security
 {
     public class Class1
     {
+
+        public int MyProperty { get; set; }
+
         public void f()
         {
             var f = 1;
+
+            System.Console.WriteLine(f);
+
         }
 
     }
